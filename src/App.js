e026f618a7caf7f4f46e3d5389f3dfa04dc8ec8b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 16:15:56 +0700

    wireup StatBySchool to router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'mobx-react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Layout from './components/Layout'
 import Main from './pages/Main'
+import StatBySchool from './pages/StatBySchool'
 import UiStore from './stores/UiStore'
 import StatStore from './stores/StatStore'
 
@@ -15,6 +16,7 @@ class App extends React.PureComponent {
           <Layout>
             <Switch>
               <Route exact path="/" component={Main} />
+              <Route path="/stat-by-school" component={StatBySchool} />
             </Switch>
             {process.env.NODE_ENV === 'development' ? <DevTools /> : null}
           </Layout>
