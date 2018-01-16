e1b43392ba322a244cd34d3d499fcbe365933bae
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 11:02:26 +0700

    add example for firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import Button from 'material-ui/Button'
+import fire from './helpers/fire'
 
 class App extends React.PureComponent {
+  componentDidMount() {
+    fire
+      .database()
+      .ref('/')
+      .on('value', s => {
+        console.log(s.val())
+      })
+  }
+
   render() {
     return (
       <Button raised color="primary">
