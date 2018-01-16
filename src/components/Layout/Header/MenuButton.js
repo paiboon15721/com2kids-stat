307c3aa9105996dbed6a9dc10f5ef16d36d7f8b0
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 14:37:52 +0700

    add MenuButton

---
+++ src/components/Layout/Header/MenuButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { withStyles } from 'material-ui/styles'
+import { inject } from 'mobx-react'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui-icons/Menu'
+
+const styles = theme => ({
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+})
+
+@inject('uiStore')
+class MenuButton extends React.Component {
+  render() {
+    const { uiStore, classes } = this.props
+    return (
+      <IconButton
+        color="contrast"
+        onClick={uiStore.openMobileNav}
+        className={classes.navIconHide}
+      >
+        <MenuIcon />
+      </IconButton>
+    )
+  }
+}
+
+export default withStyles(styles)(MenuButton)
