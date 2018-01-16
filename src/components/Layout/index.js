4392dbc3444b3db182fb50104d75d935c2e0f448
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 13:50:58 +0700

    wireup Layout component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { withStyles } from 'material-ui/styles'
+import Hidden from 'material-ui/Hidden'
+import Header from './Header'
+import { NavBarForWeb, NavBarForMobile } from './NavBar'
+
+const styles = theme => ({
+  appFrame: {
+    display: 'flex',
+  },
+  content: {
+    padding: theme.spacing.unit * 3,
+    marginTop: 56,
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      marginLeft: 240,
+    },
+    [theme.breakpoints.up('sm')]: {
+      marginTop: 64,
+    },
+  },
+})
+
+class Layout extends React.PureComponent {
+  render() {
+    const { classes, children } = this.props
+
+    return (
+      <div className={classes.appFrame}>
+        <Header />
+        <Hidden mdUp key="1">
+          <NavBarForMobile />
+        </Hidden>
+        <Hidden mdDown implementation="css" key="2">
+          <NavBarForWeb />
+        </Hidden>
+        <main className={classes.content}>{children}</main>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(Layout)
