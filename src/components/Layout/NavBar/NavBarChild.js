0c6d0c4a26c9bd8fd9a1da153085123fd23b3e7c
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 13:48:41 +0700

    add NavBarChild component

---
+++ src/components/Layout/NavBar/NavBarChild.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { withStyles } from 'material-ui/styles'
+import Typography from 'material-ui/Typography'
+import Divider from 'material-ui/Divider'
+import NavList from './NavList'
+import menuData from './menuData'
+
+const styles = theme => ({
+  drawerHeader: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...theme.mixins.toolbar,
+  },
+  title: {
+    color: 'red',
+  },
+})
+
+class NavBarChild extends React.Component {
+  render() {
+    const { classes } = this.props
+    return (
+      <div>
+        <div className={classes.drawerHeader}>
+          <Typography type="title" className={classes.title}>
+            v.0.0.1-beta.1
+          </Typography>
+        </div>
+        <Divider />
+        <NavList items={menuData} />
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(NavBarChild)
