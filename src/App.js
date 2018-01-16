7cf5217d3eb3dbcb69458908724b4eb617944118
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 14:17:07 +0700

    setup router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Layout from './components/Layout'
 import Main from './pages/Main'
 
 class App extends React.PureComponent {
   render() {
     return (
-      <Layout>
-        <Main />
-      </Layout>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Main} />
+          </Switch>
+        </Layout>
+      </Router>
     )
   }
 }
