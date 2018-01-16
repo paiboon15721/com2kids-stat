f1eb67fb48a84ad0dfbaa418a4fb9eb8cbc68c83
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 10:16:26 +0700

    initial material ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from 'material-ui/Button'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => (
+  <Button raised color="primary">
+    Hello World
+  </Button>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
