74609a22f4f1eafafa32e40deb6832f979787b00
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 16:18:01 +0700

    update menu

--- src/components/Layout/NavBar/menuData.js
+++ src/components/Layout/NavBar/menuData.js
@@ -1,1 +1,8 @@
-export default [{ primary: 'Main', path: '/main', title: 'Main Page' }]
+export default [
+  { primary: 'Main', path: '/main', title: 'Main Page' },
+  {
+    primary: 'Stat by school',
+    path: '/stat-by-school',
+    title: 'Stat by school',
+  },
+]
