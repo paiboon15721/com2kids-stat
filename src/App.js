d8cab810f849af1e67bd38065c62e1ddb7b92784
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 11:42:08 +0700

    setup table

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React from 'react'
-import Button from 'material-ui/Button'
 import {
   Grid,
   Table,
   TableHeaderRow,
 } from '@devexpress/dx-react-grid-material-ui'
+import map from 'lodash/map'
+import filter from 'lodash/filter'
+import size from 'lodash/size'
 
 import fire from './helpers/fire'
 
 class App extends React.PureComponent {
+  state = {
+    data: [],
+  }
+
   componentDidMount() {
     fire
       .database()
       .ref('/')
       .on('value', s => {
-        console.log(s.val())
+        this.setState({
+          data: map(s.val(), (v, k) => ({
+            name: k,
+            start: filter(v.usage, x => x.STARTDT !== undefined).length,
+            stop: filter(v.usage, x => x.STOPDT !== undefined).length,
+            total: size(v.usage),
+          })),
+        })
       })
   }
 
   render() {
     return (
       <Grid
-        rows={[
-          { id: 0, product: 'DevExtreme', owner: 'DevExpress' },
-          { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' },
-        ]}
+        rows={this.state.data}
         columns={[
-          { name: 'id', title: 'ID' },
-          { name: 'product', title: 'Product' },
-          { name: 'owner', title: 'Owner' },
+          { name: 'name', title: 'School name' },
+          { name: 'start', title: 'Start' },
+          { name: 'stop', title: 'Stop' },
+          { name: 'total', title: 'Total' },
         ]}
       >
         <Table />
