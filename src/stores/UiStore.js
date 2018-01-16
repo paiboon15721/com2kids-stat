2058e6ee45f4a8206d75ff8447da8feda9bb0360
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 16:47:40 +0700

    add currentPath stat to store

--- src/stores/UiStore.js
+++ src/stores/UiStore.js
@@ -2,6 +2,7 @@ import { observable, action } from 'mobx'
 
 export default new class UiStore {
   @observable showMobileNav = false
+  @observable currentPath = '/'
 
   @action.bound
   openMobileNav() {
@@ -11,5 +12,10 @@ export default new class UiStore {
   @action.bound
   closeMobileNav() {
     this.showMobileNav = false
+  }
+
+  @action.bound
+  changePath(path) {
+    this.currentPath = path
   }
 }()
