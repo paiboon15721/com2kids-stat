26c18e7ac3a3a1b9a778299cf83a504d6cd7a17d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 14:25:20 +0700

    setup devtools and wireup mobx store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import DevTools from 'mobx-react-devtools'
+import { Provider } from 'mobx-react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Layout from './components/Layout'
 import Main from './pages/Main'
+import UiStore from './stores/UiStore'
 
 class App extends React.PureComponent {
   render() {
     return (
-      <Router>
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Main} />
-          </Switch>
-        </Layout>
-      </Router>
+      <Provider uiStore={UiStore}>
+        <Router>
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Main} />
+            </Switch>
+            {process.env.NODE_ENV === 'development' ? <DevTools /> : null}
+          </Layout>
+        </Router>
+      </Provider>
     )
   }
 }
