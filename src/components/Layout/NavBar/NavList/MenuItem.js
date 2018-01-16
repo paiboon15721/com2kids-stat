e08167bd7c39069c64e97d1c0b7d1cd98f60d81c
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 16:47:19 +0700

    change route when click navbar

--- src/components/Layout/NavBar/NavList/MenuItem.js
+++ src/components/Layout/NavBar/NavList/MenuItem.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { ListItem, ListItemText, ListItemIcon } from 'material-ui/List'
 import NavigateNextIcon from 'material-ui-icons/NavigateNext'
+import { withRouter } from 'react-router-dom'
+import { inject, observer } from 'mobx-react'
 
+@withRouter
+@inject('uiStore')
+@observer
 class MenuItem extends React.Component {
   render() {
-    const { primary } = this.props
+    const { primary, path, history, uiStore } = this.props
+    const active = path === history.location.pathname
     return (
-      <ListItem button>
-        <ListItemIcon>
-          <NavigateNextIcon />
-        </ListItemIcon>
+      <ListItem
+        button
+        onClick={() => {
+          history.push(path)
+          uiStore.changePath(path)
+        }}
+      >
+        {active && (
+          <ListItemIcon>
+            <NavigateNextIcon />
+          </ListItemIcon>
+        )}
         <ListItemText inset primary={primary} />
       </ListItem>
     )
