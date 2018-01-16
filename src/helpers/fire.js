2df7a65b34dd8612dfb272ef24b62dfe331e94c8
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 11:02:13 +0700

    add firebase config

---
+++ src/helpers/fire.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+export default firebase.initializeApp({
+  apiKey: 'AIzaSyCsxH-qYnqMbiXlkjB1SLJBZreW9sUAgUw',
+  authDomain: 'com2kids-5be26.firebaseapp.com',
+  databaseURL: 'https://com2kids-5be26.firebaseio.com',
+  projectId: 'com2kids-5be26',
+  storageBucket: 'com2kids-5be26.appspot.com',
+  messagingSenderId: '56322755216',
+})
