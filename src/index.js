27b952891f23a260a919e286b82b9576b917c683
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-16 10:48:30 +0700

    wireup roboto font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from 'material-ui/Button'
+import 'typeface-roboto'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
