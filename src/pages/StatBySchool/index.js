ab3ee5c17d14a8b1f88b08918c5ae76da6bafc66
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-17 14:04:03 +0700

    remove paper

--- src/pages/StatBySchool/index.js
+++ src/pages/StatBySchool/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import Paper from 'material-ui/Paper'
 import StatChartByDateList from './StatChartByDateList'
 
 class StatBySchool extends React.PureComponent {
   render() {
-    return (
-      <Paper>
-        <StatChartByDateList />
-      </Paper>
-    )
+    return <StatChartByDateList />
   }
 }
 
