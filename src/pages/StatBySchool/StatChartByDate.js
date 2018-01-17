76cca431966a83b67bc4a3923d4be5600f9717f8
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-01-17 14:04:31 +0700

    implement StatChart

--- src/pages/StatBySchool/StatChartByDate.js
+++ src/pages/StatBySchool/StatChartByDate.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts'
 
 class FilterDate extends React.Component {
   render() {
-    return <div>Test</div>
+    const { data } = this.props
+    return (
+      <BarChart
+        width={600}
+        height={300}
+        data={data}
+        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+      >
+        <XAxis dataKey="schoolName" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="start" stackId="a" fill="#8884d8" />
+        <Bar dataKey="stop" stackId="a" fill="#82ca9d" />
+      </BarChart>
+    )
   }
 }
 
