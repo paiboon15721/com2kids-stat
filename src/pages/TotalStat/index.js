52fdb882069f71169ac0b4fa6a1aa18552b659c0
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-08 09:14:23 +0700

    update css

--- src/pages/TotalStat/index.js
+++ src/pages/TotalStat/index.js
@@ -3,23 +3,32 @@ import Paper from 'material-ui/Paper'
 import { withStyles } from 'material-ui/styles'
 import StatTable from './StatTable'
 import StatChart from './StatChart'
+import NotUseStatChart from './NotUseStatChart'
 
 const styles = theme => ({
   root: {
     display: 'flex',
-    flexDirection: 'row',
+    flexFlow: 'row wrap',
+    justifyContent: 'space-between',
     [theme.breakpoints.down('md')]: {
-      flexDirection: 'column',
+      flexFlow: 'column nowrap',
     },
   },
   statChart: {
-    marginRight: 10,
+    boxSizing: 'border-box',
     padding: 20,
     height: 400,
-    minWidth: 400,
+    width: '49%',
     [theme.breakpoints.down('md')]: {
+      width: '100%',
       marginRight: 0,
       marginBottom: 20,
+    },
+  },
+  statTable: {
+    boxSizing: 'border-box',
+    [theme.breakpoints.up('md')]: {
+      marginTop: 20,
     },
   },
 })
@@ -30,13 +39,15 @@ class TotalStat extends React.PureComponent {
     const { classes } = this.props
     return (
       <div className={classes.root}>
-        <Paper
-          className={classes.statChart}
-        >
+        <Paper className={classes.statChart}>
+          <h3>Total Usage</h3>
+          <NotUseStatChart />
+        </Paper>
+        <Paper className={classes.statChart}>
           <h3>Total Usage</h3>
           <StatChart />
         </Paper>
-        <Paper>
+        <Paper className={classes.statTable}>
           <StatTable />
         </Paper>
       </div>
