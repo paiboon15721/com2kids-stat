62cf817813cb50ebed6709c28fde5b3f5f26dc94
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-09 13:30:19 +0700

    improve css

--- src/pages/StatByDate/StatChartByDateList.js
+++ src/pages/StatByDate/StatChartByDateList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
 import { withRouter } from 'react-router-dom'
+import { withStyles } from 'material-ui/styles'
 import moment from 'moment'
 import Paper from 'material-ui/Paper'
 import range from 'lodash/range'
@@ -8,12 +9,34 @@ import filter from 'lodash/filter'
 import map from 'lodash/map'
 import StatChartByDate from './StatChartByDate'
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    justifyContent: 'space-between',
+    [theme.breakpoints.down('md')]: {
+      flexFlow: 'column nowrap',
+    },
+  },
+  statChart: {
+    boxSizing: 'border-box',
+    padding: 10,
+    height: 400,
+    width: '49%',
+    marginBottom: 10,
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+    },
+  },
+})
+
+@withStyles(styles)
 @withRouter
 @inject('statStore')
 @observer
 class StatChartByDateList extends React.Component {
-  render() {
-    const { statStore, match } = this.props
+  renderStatList() {
+    const { statStore, match, classes } = this.props
     const back = match.params.back || 10
     const statByDate = map(range(back), n => {
       const date = moment()
@@ -53,14 +76,16 @@ class StatChartByDateList extends React.Component {
       }
     })
     return map(statByDate, x => (
-      <Paper
-        style={{ padding: 10, marginBottom: 10, height: 400 }}
-        key={x.date}
-      >
+      <Paper className={classes.statChart} key={x.date}>
         <h3>Date: {x.date}</h3>
         <StatChartByDate data={x.data} />
       </Paper>
     ))
+  }
+
+  render() {
+    const { classes } = this.props
+    return <div className={classes.root}>{this.renderStatList()}</div>
   }
 }
 
