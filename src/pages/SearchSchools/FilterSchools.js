644877701bb03c789f5d7b89e657d141c1968c3d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-09 16:17:11 +0700

    initial FilterSchools component

---
+++ src/pages/SearchSchools/FilterSchools.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Paper from 'material-ui/Paper'
+
+class FilterSchools extends React.PureComponent {
+  render() {
+    return <Paper>Test</Paper>
+  }
+}
+
+export default FilterSchools
