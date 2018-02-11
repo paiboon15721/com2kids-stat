66eb2f8e5d32e65809703c64e53e78cf4b319407
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-11 21:02:40 +0700

    implement SelectProvince feature

--- src/pages/SearchSchools/FilterSchools/SelectProvince.js
+++ src/pages/SearchSchools/FilterSchools/SelectProvince.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import Select from 'react-select'
+import map from 'lodash/map'
+import { inject, observer } from 'mobx-react'
 
-class SelectProvince extends React.PureComponent {
-  state = {
-    selectedOption: '',
+@inject('schoolStore')
+@observer
+class SelectProvince extends React.Component {
+  componentDidMount() {
+    this.props.schoolStore.fetchSchoolProvinces()
   }
 
   handleChange = selectedOption => {
-    this.setState({ selectedOption })
+    this.props.schoolStore.selectProvince(selectedOption)
   }
 
   render() {
-    const { selectedOption } = this.state
-    const value = selectedOption && selectedOption.value
+    const { selectedProvince, schoolProvinces } = this.props.schoolStore
+    const value = selectedProvince && selectedProvince.value
+    const options = map(schoolProvinces, v => ({ value: v, label: v }))
 
     return (
       <Select
         name="form-field-name"
         value={value}
         onChange={this.handleChange}
-        options={[
-          { value: 'one', label: 'One' },
-          { value: 'two', label: 'Two' },
-        ]}
+        options={options}
       />
     )
   }
