2f4799a56e9cea9e6d845b607d59f8acffebc025
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-11 23:55:51 +0700

    add styling

--- src/pages/SearchSchools/FilterSchools/index.js
+++ src/pages/SearchSchools/FilterSchools/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
 import SelectProvince from './SelectProvince'
+import SearchButton from './SearchButton'
+
+const styles = {
+  root: { padding: 16 },
+}
 
 class FilterSchools extends React.PureComponent {
   render() {
     return (
-      <Paper>
+      <Paper style={styles.root}>
         <SelectProvince />
+        <SearchButton />
       </Paper>
     )
   }
