07881448d33e4c796e80aa9c07e304e56af496ab
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-11 23:56:22 +0700

    use mobx instend

--- src/pages/SearchSchools/SchoolsTable.js
+++ src/pages/SearchSchools/SchoolsTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
+import { inject, observer } from 'mobx-react'
 import { PagingState, CustomPaging } from '@devexpress/dx-react-grid'
 import {
   Grid,
@@ -7,99 +8,49 @@ import {
   TableHeaderRow,
   PagingPanel,
 } from '@devexpress/dx-react-grid-material-ui'
-import axios from 'axios'
-import toInteger from 'lodash/toInteger'
 import map from 'lodash/map'
 import Loading from './Loading'
-import { baseUrl } from '../../helpers/config'
 
-const URL = `${baseUrl}/schools`
-
-class SchoolsTable extends React.PureComponent {
-  state = {
-    columns: [
-      { name: 'name', title: 'Name' },
-      { name: 'province', title: 'Province' },
-      { name: 'type', title: 'Type' },
-      { name: 'size', title: 'Size' },
-      { name: 'totalStudent', title: 'Total Student' },
-      { name: 'totalTeacher', title: 'Total Teacher' },
-      { name: 'usable', title: 'Usable' },
-    ],
-    rows: [],
-    totalCount: 0,
-    pageSize: 10,
-    currentPage: 0,
-    loading: true,
-  }
-
-  componentDidMount() {
-    this.loadData(1)
-  }
-
-  componentDidUpdate() {
-    this.loadData()
-  }
+@inject('schoolStore')
+@observer
+class SchoolsTable extends React.Component {
+  columns = [
+    { name: 'name', title: 'Name' },
+    { name: 'province', title: 'Province' },
+    { name: 'type', title: 'Type' },
+    { name: 'size', title: 'Size' },
+    { name: 'totalStudent', title: 'Total Student' },
+    { name: 'totalTeacher', title: 'Total Teacher' },
+    { name: 'usable', title: 'Usable' },
+  ]
 
   changeCurrentPage = currentPage => {
-    this.setState({
-      loading: true,
-      currentPage,
-    })
+    const { schoolStore } = this.props
+    schoolStore.currentPage = currentPage
+    schoolStore.fetchSchools()
   }
 
-  queryString() {
-    const { pageSize, currentPage } = this.state
-    return `${URL}?limit=${pageSize}&skip=${pageSize * currentPage}`
-  }
-
-  loadData = async (count = 0) => {
-    const queryString = this.queryString()
-    if (queryString === this.lastQuery) {
-      this.setState({ loading: false })
-      return
-    }
-    this.lastQuery = queryString
-    try {
-      const { data, headers } = await axios.get(`${queryString}&count=${count}`)
-      const rows = map(data, v => ({
-        name: v.name,
-        province: v.province,
-        type: v.type,
-        size: v.size,
-        totalStudent: v.totalStudent,
-        totalTeacher: v.totalTeacher,
-        usable: v.assets.computerTotal.usable,
-      }))
-      if (this.state.totalCount === 0) {
-        this.setState({
-          rows,
-          totalCount: toInteger(headers['x-total-count']),
-          loading: false,
-        })
-      } else {
-        this.setState({
-          rows,
-          loading: false,
-        })
-      }
-    } catch (err) {
-      this.setState({ loading: false })
-    }
-  }
   render() {
     const {
-      rows,
-      columns,
-      pageSize,
-      currentPage,
+      schools,
       totalCount,
       loading,
-    } = this.state
+      currentPage,
+      pageSize,
+    } = this.props.schoolStore
+    const rows = map(schools, v => ({
+      name: v.name,
+      province: v.province,
+      type: v.type,
+      size: v.size,
+      totalStudent: v.totalStudent,
+      totalTeacher: v.totalTeacher,
+      usable: v.assets.computerTotal.usable,
+    }))
 
     return (
       <Paper style={{ position: 'relative' }}>
-        <Grid rows={rows} columns={columns}>
+        <Grid rows={rows} columns={this.columns}>
           <PagingState
             currentPage={currentPage}
             onCurrentPageChange={this.changeCurrentPage}
--- src/stores/SchoolStore.js
+++ src/stores/SchoolStore.js
@@ -1,8 +1,16 @@
 import { observable, action, runInAction } from 'mobx'
 import axios from 'axios'
+import queryString from 'query-string'
+import toInteger from 'lodash/toInteger'
 import { baseUrl } from '../helpers/config'
 
 export default new class SchoolStore {
+  q = {}
+  pageSize = 10
+  @observable loading = false
+  @observable schools = []
+  @observable totalCount = 0
+  @observable currentPage = 0
   @observable schoolProvinces = []
   @observable selectedProvince = ''
 
@@ -14,8 +22,32 @@ export default new class SchoolStore {
     })
   }
 
+  @action.bound
+  async fetchSchools(count = 0) {
+    this.loading = true
+    if (count) {
+      this.currentPage = 0
+    }
+    this.q.limit = this.pageSize
+    this.q.skip = this.pageSize * this.currentPage
+    const qStr = queryString.stringify(this.q)
+    console.log(`${baseUrl}/schools?count=${count}&${qStr}`)
+    const { data, headers } = await axios.get(
+      `${baseUrl}/schools?count=${count}&${qStr}`,
+    )
+    runInAction(() => {
+      this.schools = data
+      const xTotalCount = toInteger(headers['x-total-count'])
+      if (xTotalCount !== 0) {
+        this.totalCount = xTotalCount
+      }
+      this.loading = false
+    })
+  }
+
   @action
   selectProvince(province) {
     this.selectedProvince = province
+    this.q.province = province.value
   }
 }()
