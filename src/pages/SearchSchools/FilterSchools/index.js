f271bfb77fcd4d6700bb96af956999abbab97c6d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-11 20:23:41 +0700

    wireup SelectProvince

--- src/pages/SearchSchools/FilterSchools/index.js
+++ src/pages/SearchSchools/FilterSchools/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
+import SelectProvince from './SelectProvince'
 
 class FilterSchools extends React.PureComponent {
   render() {
-    return <Paper>Test</Paper>
+    return (
+      <Paper>
+        <SelectProvince />
+      </Paper>
+    )
   }
 }
 
