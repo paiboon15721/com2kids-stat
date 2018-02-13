ffb6369e758c3f7aa3f5120e616f3ef581056804
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-13 15:14:17 +0700

    fix bug

--- src/pages/SearchSchools/FilterSchools/SelectProvince.js
+++ src/pages/SearchSchools/FilterSchools/SelectProvince.js
@@ -11,7 +11,7 @@ class SelectProvince extends React.Component {
   }
 
   handleChange = selectedOption => {
-    this.props.schoolStore.q.province = selectedOption.value
+    this.props.schoolStore.q.province = selectedOption && selectedOption.value
   }
 
   render() {
