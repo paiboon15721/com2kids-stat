d3fbd004fed0824130558c2b725a685b73e72539
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2018-02-14 14:43:32 +0700

    implement RowDetail component

--- src/pages/SearchSchools/SchoolsTable/RowDetail.js
+++ src/pages/SearchSchools/SchoolsTable/RowDetail.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import map from 'lodash/map'
+import flat from 'flat'
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    justifyContent: 'space-between',
+  },
+  item: {
+    width: '25%',
+    marginBottom: '0.5rem',
+  },
+  key: {
+    display: 'inline-block',
+  },
+}
+
+const Item = ({ k, v }) => (
+  <div style={styles.item}>
+    <b style={styles.key}>{k}:</b> <font color="red">{v}</font>
+  </div>
+)
 
 class RowDetail extends React.PureComponent {
+  renderItemsList() {
+    return map(flat(this.props.row), (v, k) => {
+      return <Item key={k} k={k} v={v} />
+    })
+  }
+
   render() {
-    const { row } = this.props
-    return <div>test {row.assets.computerTotal.usable}</div>
+    return <div style={styles.root}>{this.renderItemsList()}</div>
   }
 }
 
